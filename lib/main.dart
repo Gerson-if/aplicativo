fbb1247cca85303c388a2e927a0b42ec48ba1f47
Gerson-if <gersodecastro@gmail.com>
2023-12-23 19:06:07 -0300

    Aula inicial_flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Aplicativo do Gerson",
+      home: Row(
+        children: <Widget>[
+          Text(" Usuario: "),
+          Text(" Senha: "),
+          Text(" esqueci minha senha..."),
+        ],
+      )));
+}
